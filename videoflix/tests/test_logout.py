b0ed2531ed89bffc2634e95ba0f193c2df3d69b0
Christoph Völker <christov@gmx.de>
2025-05-04 21:19:59 +0200

    login logout tests

--- videoflix/tests/test_logout.py
+++ videoflix/tests/test_logout.py
@@ -32,8 +32,6 @@ class LoginTests(APITestCase):
             match, "Kein Verification Code in E-Mail gefunden")
         self.verification_code = match.group(1)
 
-    def test_login_success(self):
-
         # verify first
         # 👇 Bestätigungsrequest absenden
         url = reverse('authemail-signup-verify')
@@ -50,17 +48,29 @@ class LoginTests(APITestCase):
         }
         response = self.client.post(url, data, format="json")
 
-        #print(response.data)
+        # print(response.data)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_login_not_verified(self):
+        # 👇 Token setzen
+        self.token = response.data["token"]
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token}")
 
-        url = reverse('authemail-login')
-        data = {
-            "email": self.email,
-            "password": self.password,
-        }
-        response = self.client.post(url, data, format="json")
+    def test_logout(self):
 
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        url = reverse('authemail-logout')
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logout_invalidates_token(self):
+        # Erst Logout
+        url = reverse('authemail-logout')
+        self.client.get(url)
+
+        # Dann versuchen, mit altem Token geschützte Route aufzurufen
+        url = reverse('authemail-me')  # z. B. geschützter Endpoint
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
