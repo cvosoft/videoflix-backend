1c6abf4559d8a0adffc266cca8cf5e5b6bd20704
Christoph Völker <christov@gmx.de>
2025-05-23 10:53:11 +0200

    cors update

--- videoflix/settings/dev.py
+++ videoflix/settings/dev.py
@@ -9,3 +9,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 FILE_UPLOAD_MAX_MEMORY_SIZE = 5 * 1024 * 1024 * 1024  # 5 GB
 DATA_UPLOAD_MAX_MEMORY_SIZE = 5 * 1024 * 1024 * 1024  # 5 GB
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
+]
--- videoflix/settings/prod.py
+++ videoflix/settings/prod.py
@@ -10,3 +10,8 @@ MEDIA_URL = '/media/'
 FILE_UPLOAD_TEMP_DIR = '/mnt/blockstorage/tmp_uploads'
 FILE_UPLOAD_MAX_MEMORY_SIZE = 5 * 1024 * 1024 * 1024  # 5 GB
 DATA_UPLOAD_MAX_MEMORY_SIZE = 5 * 1024 * 1024 * 1024  # 5 GB
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://predigtflix.de",
+]
